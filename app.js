f69e3135a59b544228742070390251f3e6670490
bprudencio-dev <bprudencio.dev@gmail.com>
2023-08-28 11:12:55 +0100

    Express08 challenge done but bonus not done

--- app.js
+++ app.js
@@ -12,21 +12,36 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const { hashPassword, verifyPassword, verifyToken } = require('./auth');
+const usersHandlers = require('./usersHandlers');
 
+//Public Routes
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-
-const usersHandlers = require('./usersHandlers');
-const { hashPassword } = require('./auth');
-
+app.post("/api/login", usersHandlers.getUserByEmailWithPasswordAndPassToNext, verifyPassword);
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersId);
 app.post("/api/users", hashPassword, usersHandlers.postUser)
+
+//Protected routes
+app.use(verifyToken);
+app.post("/api/movies", verifyToken, movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
+
 app.put("/api/users/:id", hashPassword,  usersHandlers.updateUser)
 app.delete("/api/users/:id", usersHandlers.deleteUser)
+
+// const isItDwight = (req, res)=>{
+//   if(req.body.email ===  "dwight@theoffice.com" && req.body.password === "123456"){
+//     res.send('Credentials are valid');
+//   }else {
+//     res.status(401).send('Credentials are incorrect')
+//   }
+// };
+
+
 
 
 
--- usersHandlers.js
+++ usersHandlers.js
@@ -97,6 +97,25 @@ const updateUser = (req, res) => {
 
     }
 
+    const getUserByEmailWithPasswordAndPassToNext =(req, res, next)=>{
+        const{email}= req.body;
+        database
+            .query("SELECT * FROM users WHERE email=?",[email])
+            .then(([users])=>{
+                if(users[0] !== null){
+                    req.user = users[0];
+
+                    next();
+                }else{
+                    res.sendStatus(401)
+                }
+            })
+            .catch((err)=>{
+                console.error(err);
+                res.status(500).send('Error retrieving data from database')
+            })
+    }
+
 
 module.exports = {
     getUsers,
@@ -104,4 +123,5 @@ module.exports = {
     postUser,
     updateUser,
     deleteUser,
+    getUserByEmailWithPasswordAndPassToNext,
   };
